8f262fd3775d9dd9ebef5142308aea015560c050
ernestkkb <ernest.khoo.2018@smu.edu.sg>
2021-11-25 18:11:09 +0800

    Set up of backend and frontend

---
+++ demo/src/main/java/com/match/demo/user/User.java
@@ -0,0 +1,52 @@
+package com.match.demo.user;
+
+import java.util.Date;
+
+public class User {
+
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Date createdAt;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+}
